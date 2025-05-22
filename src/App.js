edc54bf379dd75b794268a1ae50a67c3dc2e3604
khantnaythwayzaw <khantnay55555@gmail.com>
2025-05-22 12:45:26 +0630

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
+import { useState } from "react";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignInForm />
     </div>
   );
 }
 
-export default App;
+function SignInForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [re_password, setRePassword] = useState("");
+  const passwordCorrect = password === re_password;
+  function handleRePasswordChange(event) {
+    setRePassword(event.target.value);
+  }
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!username.trim() || !password.trim()) {
+      return;
+    }
+    if (!passwordCorrect) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    const userInput = { username, password };
+
+    axios
+      .post("http://localhost:8080/restapi/signin.php", userInput)
+      .then((response) => {
+        if (response.data.status === 1) {
+          alert("User registered successfully.");
+        } else {
+          alert(response.data.message); // e.g., "Username already exists."
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred while connecting to the server.");
+      });
+  }
+
+  return (
+    <div className="sign-in-form">
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" onChange={handleUsernameChange} />
+        </label>
+        <label>
+          Password:
+          <input
+            className={passwordCorrect ? "" : "pwIncorrect"}
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            className={passwordCorrect ? "" : "pwIncorrect"}
+            type="password"
+            name="re_password"
+            onChange={handleRePasswordChange}
+          />
+        </label>
+        <button type="submit" onClick={handleSubmit}>
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+}
